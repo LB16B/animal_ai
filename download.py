747bac94511c93a5cd181b33c54c32671204e6a9
LB16B <ogawa.yui2000@gmail.com>
2023-06-13 14:37:05 +0900

    download.py

--- download.py
+++ download.py
@@ -2,3 +2,29 @@ from flickrapi import FlickrAPI
 from urllib.request import urlretrieve
 from pprint import pprint
 import os, time, sys
+
+
+# APIの情報
+
+key = "2c0fcba614339e234d9cb7285a1161f2"
+secret = "8443be2a6d1ea74d"
+wait_time = 1
+
+
+# 保存フォルダの指定
+
+animalname = sys.argv[1]
+savedir = "./" + animalname
+
+flickr = FlickrAPI(key, secret, format='parsed-json')
+result = flickr.photos.search(
+    text = animalname,
+    per_page = 400,
+    media = 'photos',
+    sort = 'relevance',
+    safe_search = 1,
+    extras = 'url_q, licence'
+)
+
+photos = result['photos']
+pprint(photos)
